9ab84ec0af674fe0e6b358d32fe5996b1809f39d
homi <275571952@qq.com>
2018-01-02 19:54:04 +0800

    Signed-off-by: homi <275571952@qq.com>

---
+++ config/default.js
@@ -0,0 +1,12 @@
+/**
+ * Created by hasee on 2017/11/24.
+ */
+module.exports = {
+  port: 3000,
+  session: {
+    secret: 'myblog',
+    key: 'myblog',
+    maxAge: 2592000000
+  },
+  mongodb: 'mongodb://localhost:27017/myblog'
+}
